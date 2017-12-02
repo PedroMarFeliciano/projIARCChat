7e916a3e2c641cac17e6d3e074eeac226f02e5a5
Pedro Feliciano <pedro.mar.feliciano@gmail.com>
2017-12-02 20:36:38 +0000

    Implementação do código que garante que os apelidos são únicos Dict user: socket

--- client.py
+++ client.py
@@ -1,12 +1,41 @@
-import socket, sys, select
+import socket, sys, select, pickle, os
 
-server_address = ('localhost', 50055)
+def clear():
+	os.system("clear")
 
-nickname = input("Digite seu apelido: ")
+server_address = ('localhost', 50051)
 
 server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_connection.connect(server_address)
 
+unique = False
+
+while  not unique:
+	nickname = input("Digite seu apelido: ")
+	# envia o nickname para o servidor
+	server_connection.send(nickname.encode())
+
+	unique_ser = server_connection.recv(4096)
+	unique = pickle.loads(unique_ser)
+
+	if not unique:
+		clear()
+		print("Apelido em utilização. Escolha outro.")
+
+clear()
+print("Bem vindo(a) ao chat,", nickname + '.')
+
+user_list_ser = server_connection.recv(4096)
+user_list = pickle.loads(user_list_ser)
+
+print("Existem", len(user_list), "utilizadores online.\nUtilizadores:")
+
+for user in user_list:
+	print(user)
+
+print("\nPara enviar uma mensagem privada digite .<nomeDoUtilizador> <mensagem>")
+
+#lista de sockets ouvida pelo select
 sockets = [server_connection, sys.stdin]
 
 while True:
@@ -15,13 +44,18 @@ while True:
 	for s in read_sockets:
 		# Mensagem recebida
 		if s is server_connection:
-			msg = s.recv(1024).decode()
+			clear()
+			serialized_msg = s.recv(4096)
 
-			if not msg:
+			msgs = pickle.loads(serialized_msg)
+
+			if not msgs:
 				print("Problema com o servidor.")
 				sys.exit(1)
 			else:
-				print(msg)
+
+				for m in msgs:
+					print(m)
 
 		else:
 			msg = nickname + " diz: " + input()
--- server.py
+++ server.py
@@ -1,13 +1,25 @@
 import select, socket, pickle
+from datetime import datetime
+
+
+def writeToFile(chatMessages):
+    instant = datetime.now()
+    date = "["+str(instant.day)+"/"+str(instant.month)+"/"+str(instant.year)+"-"
+    date = date +str(instant.hour)+":"+str(instant.minute)+":"+str(instant.second)+"]"
+
+    with open("chat_history.txt", "a") as f:
+
+        f.write(date + " - " + chatMessages + "\n")
+        f.close()
 
 # Cria socket TCP/IP
 try:
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setblocking(0)
 except socket.error:
-    print("Erro no socket. ")
+    print("Erro no socket.")
 
-server_address = ('localhost', 50055)
+server_address = ('localhost', 50051)
 print ('starting up on: %s, port: %s' %server_address)
 
 
@@ -22,6 +34,12 @@ outputs = []
 # fila de mensagens para enviar
 messages = []
 
+# dicionario que lista o apelido dos usuarios e seus sockets
+users = {}
+
+# lista com o nome dos usuarios
+users_list = []
+
 while inputs:
     readable, writable, exceptional = select.select(inputs, outputs, inputs)
 
@@ -30,20 +48,45 @@ while inputs:
         if sock is server:
             connection, client_adress = sock.accept()
             print("Nova conexao de ", client_adress)
+
+            unique = False
+
+            while not unique:
+                new_user_nickname = connection.recv(4096).decode()
+
+                if new_user_nickname not in users:
+                    unique = True
+
+                connection.send(pickle.dumps(unique))
+
+            # guarda o apelido do novo usuario e também seu socket
+            users[new_user_nickname] = connection
+            users_list.append(new_user_nickname)
+
+            for u in users_list:
+                print(u)
             connection.setblocking(0)
+            # envia a lista de usuarios conectados para o cliente
+            users_list_ser = pickle.dumps(users_list)
+            print("aqui: ", users_list_ser)
+            connection.send(users_list_ser)
+            
+
             inputs.append(connection)
             outputs.append(connection)
             
         # caso nao seja o socket do servidor, e uma mensagem
         else:
-            data = sock.recv(1024).decode()
-            messages.append(data)
-
-            #serialized_msgs = pickle
+            data = sock.recv(4096).decode()
 
             if data:
-                #print("Recebi a mensagem '%s' de '%s'" % (data, sock.getpeername()))
-                        
+                print("Recebi a mensagem '%s' de '%s'" % (data, sock.getpeername()))
+                
+                messages.append(data)
+
+                writeToFile(data)
+
+                serialized_msgs = pickle.dumps(messages)
+
                 for s in outputs:
-                    s.send(data.encode())
-            
+                    s.send(serialized_msgs)
